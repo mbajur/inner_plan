8dbcf4d7e2357da4202b644f16689cdb3de1dc26
mbajur <mbajur@gmail.com>
2024-12-05 22:03:16 +0100

    namespace javascript controllers

--- app/assets/config/inner_plan_manifest.js
+++ app/assets/config/inner_plan_manifest.js
@@ -1,5 +1,5 @@
 //= link_directory ../stylesheets/inner_plan .css
 //= link_tree  ../../javascript
-//= link controllers/lists_controller.js
-//= link controllers/groups_controller.js
-//= link controllers/tasks_controller.js
+//= link controllers/inner_plan/lists_controller.js
+//= link controllers/inner_plan/groups_controller.js
+//= link controllers/inner_plan/tasks_controller.js
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,10 @@
 import { Application } from "@hotwired/stimulus"
 import * as bootstrap from 'bootstrap'
 
-import ListsController from "./controllers/lists_controller"
-import GroupsController from "./controllers/groups_controller"
-import TasksController from "./controllers/tasks_controller"
-import TaskInlineFormController from "./controllers/task_inline_form_controller"
+import ListsController from "./controllers/inner_plan/lists_controller"
+import GroupsController from "./controllers/inner_plan/groups_controller"
+import TasksController from "./controllers/inner_plan/tasks_controller"
+import TaskInlineFormController from "./controllers/inner_plan/task_inline_form_controller"
 import HwComboboxController from "controllers/hw_combobox_controller"
 
 // Stimulus
