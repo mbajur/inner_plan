f1f15bdeb068d01c2a5ce7d7fefa50512a81912a
mbajur <mbajur@gmail.com>
2024-12-10 11:02:16 +0100

    fix

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,6 +1,6 @@
 RSpec.configure do |config|
   # TODO Remove when Devise fixes https://github.com/heartcombo/devise/issues/5705
   config.before(:each, type: :system) do
-    Rails.application.reload_routes_unless_loaded
+    Rails.application.reload_routes_unless_loaded if Rails.application.respond_to?(:reload_routes_unless_loaded)
   end
 end
